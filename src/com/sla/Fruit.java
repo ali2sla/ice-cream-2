be3327af4e6223d85c73796e38f89325198061f9
alanli <alli19@scienceleadership.org>
2018-09-20 10:30:33 -0400

    Test.

--- src/com/sla/Fruit.java
+++ src/com/sla/Fruit.java
@@ -4,12 +4,16 @@ class Fruit {
     String name;
     int calories;
     String type;
+    int size;
+    int price;
+    int tax;
     boolean organic;
 
     void explains() {
         System.out.println("Name: " + name);
         System.out.println("Calories: " + calories);
         System.out.println("Type: " + type);
+        System.out.println("Serving size: " + size);
         if (organic) {
             System.out.println("It is organic");
         } else {
@@ -17,7 +21,16 @@ class Fruit {
         }
     }
 
+    void buy() {
+        int cost;
+        cost = price + price*tax;
+        System.out.println("It will cost $" + cost +" for Bob's Famous " + name + ".");
+        }
+
     int calories() {
         return calories;
+    }
+    int size() {
+        return size;
     }
 }
--- src/com/sla/Main.java
+++ src/com/sla/Main.java
@@ -1,6 +1,6 @@
 package com.sla;
 
-class Main {
+class Snackshop {
 
     public static void main(String[] args) {
 
@@ -9,13 +9,46 @@ class Main {
         cherry.name = "Cherry";
         cherry.calories = 77;
         cherry.type = "Fruit";
+        cherry.size = 1;
+        cherry.price = 6;
+        cherry.tax = 8/100;
         cherry.organic = true;
 
         Fruit apple = new Fruit();
         apple.name = "Apple";
         apple.calories = 95;
         apple.type = "Fruit";
-        cherry.organic = false;
+        apple.size = 1;
+        apple.price = 1;
+        apple.tax = 8/100;
+        apple.organic = false;
+
+        Fruit pineapple = new Fruit();
+        pineapple.name = "Pineapple";
+        pineapple.calories = 452;
+        pineapple.type = "Fruit";
+        pineapple.size = 1;
+        pineapple.price = 4;
+        pineapple.tax = 8/100;
+        pineapple.organic = true;
+
+        Fruit cantaloupe = new Fruit();
+        cantaloupe.name = "Cantaloupe";
+        cantaloupe.calories = 186;
+        cantaloupe.type = "Fruit";
+        cantaloupe.size = 1;
+        cantaloupe.price = 7;
+        cantaloupe.tax = 8/100;
+        cantaloupe.organic = true;
+
+        Fruit orange = new Fruit();
+        orange.name = "Orange";
+        orange.calories = 186;
+        orange.type = "Fruit";
+        orange.size = 1;
+        orange.price = 7;
+        orange.tax = 8/100;
+        orange.organic = true;
 
         // List of Dessert
         Dessert sundae = new Dessert();
@@ -34,11 +67,26 @@ class Main {
         System.out.println("Information:");
         cherry.explains();
         System.out.println();
+        apple.explains();
+        System.out.println();
+        pineapple.explains();
+        System.out.println();
         sundae.explains();
+        System.out.println();
+        recake.explains();
+        System.out.println();
+
+        // Get cost
+        System.out.println("Receipt:");
+        cherry.buy();
+        System.out.println();
+        apple.buy();
+        System.out.println();
+        pineapple.buy();
         System.out.println();
 
         // Check total calories
-        int totalcalories = cherry.calories() + sundae.calories + recake.calories;
+        int totalcalories = cherry.calories + apple.calories + pineapple.calories + cantaloupe.calories + orange.calories + sundae.calories + recake.calories;
         System.out.println("The total number of calories is " + totalcalories + ".");
     }
 }
